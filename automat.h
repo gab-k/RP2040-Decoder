6070af4b1018306d270cf98b11d1590bcf91b1b6
GabrielKoppenstein <gabriel@koppenstein.com>
2022-01-04 21:47:38 +0100

    Deprecated files, not needed anymore

--- automat.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by r3d on 8/22/21.
-//
-
-#ifndef PICO_EXAMPLES_AUTOMAT_H
-#define PICO_EXAMPLES_AUTOMAT_H
-int fsm_main(const bool *arr, int index, const int len);
-#endif //PICO_EXAMPLES_AUTOMAT_H
-
